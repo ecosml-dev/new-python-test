be3ddecf6f8afae6c1d9c6d457ab036d5d1565af
EcoSML Admin <admin@ecosml.org>
2018-03-13 02:43:38 +0000

    changes

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "ecosml_new-python-test",
+    version = "0.0.1",
+    author = "{{author}}",
+    description = ("{{overview}}"),
+    license = "MIT",
+    keywords = "testing test test",
+    url = "http://localhost:3000/package/3bd2f904-7b53-427c-99cd-6b65dc33a418",
+    packages=['new-python-test'],
+    long_description=read('README.md'),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+)
