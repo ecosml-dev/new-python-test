c6d00b9d4dead1df8d036a22ec814cd64c08cdb9
EcoSML Admin <admin@ecosml.org>
2018-03-17 18:25:05 +0000

    switching to python friendly names

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "new-python-test",
+    name = "new_python_test",
     version = "0.0.1",
     author = "jrmerz",
-    description = ("this is a test, this is only a tests"),
+    description = ("this is a test, this is only a test"),
     license = "MIT",
     keywords = "testing test test",
     url = "http://localhost:3000/package/3bd2f904-7b53-427c-99cd-6b65dc33a418",
-    packages=['new-python-test'],
+    packages=['new_python_test'],
     long_description=read('README.md'),
     classifiers=[
         "License :: OSI Approved :: MIT License",
