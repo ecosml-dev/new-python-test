71b0d3be4174842c752d66c2ca100367e1aa2f5b
EcoSML Admin <admin@ecosml.org>
2018-03-17 18:14:34 +0000

    change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name = "new-python-test",
     version = "0.0.1",
     author = "jrmerz",
-    description = ("this is a test, this is only a test"),
+    description = ("this is a test, this is only a tests"),
     license = "MIT",
     keywords = "testing test test",
     url = "http://localhost:3000/package/3bd2f904-7b53-427c-99cd-6b65dc33a418",
