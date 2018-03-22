c7a2f4f6abf71e3b503c5a7b4475d8a7edb25f96
EcoSML Admin <admin@ecosml.org>
2018-03-22 15:19:40 +0000

    updating

--- setup.py
+++ setup.py
@@ -8,16 +8,17 @@ setup(
     name = "new_python_test",
     version = "0.0.1",
     author = "jrmerz",
-    description = ("this is a test, this is only a test!"),
+    description = ("this is a test, this is only a test"),
     license = "MIT",
     keywords = "testing test test",
     url = "http://localhost:3000/package/3bd2f904-7b53-427c-99cd-6b65dc33a418",
     packages=[
         'new_python_test',
-        'new_python_test.coefficients',
-        'new_python_test.examples',
         'new_python_test.main'
     ],
+    package_data={
+      'new_python_test' : ['coefficients/*']
+    },
     long_description=read('README.md'),
     classifiers=[
         "License :: OSI Approved :: MIT License",
